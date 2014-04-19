62b887aa12705bc32b63b4bf2f4202fa2ec8dc92
Akkana Peck <akkana@shallowsky.com>
2014-04-19 17:50:29 -0600

    Lots of changes for PiDoorbell that didn't get into that repo before the workshop

--- rpi/HC_SR04.py
+++ rpi/HC_SR04.py
@@ -19,6 +19,18 @@
 #
 
 # Adapted from code by Matt Hawkins of RaspberryPi-Spy.co.uk
+# Wire the circuit as shown in
+# http://www.raspberrypi-spy.co.uk/2013/01/ultrasonic-distance-measurement-using-python-part-2/
+#
+#  Modifications:
+#
+#      Author: Rupa Dachere
+#      Date: March 19, 2014
+#      Changes: 
+#    	    	1. Fixed typo for call to measure_distance_cm()
+#		2. Created measure_distance_in() for inches
+#		3. Initialized stop in measure_distance_cm() and measure_distance_in()
+
 
 import RPi.GPIO as GPIO
 import time
@@ -30,6 +42,7 @@ def init_HC_SR04():
     '''Call this once at the beginning of the script
        before measuring any distances.
     '''
+    
     # Use BCM instead of physical pin numbering:
     GPIO.setmode(GPIO.BCM)
 
@@ -40,35 +53,50 @@ def init_HC_SR04():
     # Initialize trigger to low:
     GPIO.output(GPIO_TRIGGER, False)
 
-def measure_distance_cm():
+def measure_distance_cm(verbose=False):
     '''Measure a single distance, in cemtimeters.
     '''
+    return measure_distance_in(verbose) * 2.54
+
+def measure_distance_in(verbose=False):
+    '''Measure a single distance, in inches.
+    '''
+    if verbose:
+        print "Sending trigger pulse ..."
     GPIO.output(GPIO_TRIGGER, True)
     time.sleep(0.00001)
     GPIO.output(GPIO_TRIGGER, False)
-    start = time.time()
+    if verbose:
+        print "Waiting for ECHO to go low:"
 
+    start = time.time()
     while GPIO.input(GPIO_ECHO) == 0:
         start = time.time()
 
+    if verbose:
+        print "Waiting for ECHO to go high:"
+    stop = time.time()
     while GPIO.input(GPIO_ECHO) == 1:
         stop = time.time()
+    if verbose:
+        print "Got the echo."
 
-    # Convert to centimeters:
-    return ((stop - start) * 34300)/2
+    # Convert to inches:
+    return (((stop - start) * 34300)/2)*0.393701
 
-def average_distance(samples=3):
+def average_distance(samples=3, verbose=False):
     tot = 0.0
     for i in xrange(samples):
-        tot += measure_distance()
+        tot += measure_distance_in(verbose)
         time.sleep(0.1)
     return tot / samples
 
 if __name__ == '__main__':
     try:
+        print "Initializing the rangefinder ..."
         init_HC_SR04()
         while True:
-            print "Distance: %.1f" % average_distance()
+            print "Distance: %.1f inches" % average_distance(verbose=True)
             time.sleep(1)
     except KeyboardInterrupt:
         # User pressed CTRL-C: reset GPIO settings.
