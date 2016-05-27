9ae575e3e44fdef23a59996642f512918f6cc185
Akkana Peck <akkana@shallowsky.com>
2016-05-26 18:00:15 -0600

    Allow copying of podcasts to a post-KitKat Android device, using adb, instead of just a nice simple usb-storage MP3 player. (My MP3 player died, have to use my phone instead. :-( )

---
+++ androidfiles.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import sys, os
+import subprocess
+import posixpath
+
+def is_android(path):
+    return path.startswith("android:")
+
+def strip_schema(path):
+    '''Strip off any android: prefix in the path.
+    '''
+    if path.startswith("android:"):
+        return path[8:]
+    return path
+
+def listdir(path, sort=True, sizes=False):
+    '''List the contents of the given directory.
+       Returns a list of filenames if sizes=False.
+       If sizes=True, returns a list of tuples (filename, int size).
+    '''
+    proc = subprocess.Popen(["adb", "shell", "ls", "-l", strip_schema(path)],
+                            shell=False,
+                            stdout=subprocess.PIPE)
+    stdout_lines = proc.communicate()[0].split('\n')
+    file_list = []
+    for l in stdout_lines:
+        l = l.strip().split()
+        if len(l) == 7:
+            if sizes:
+                try:
+                    file_list.append((l[-1], int(l[3])))
+                except:
+                    # This could happen for the initial "Total:" line
+                    pass
+            else:
+                file_list.append(l[-1])
+    if sort:
+        file_list.sort()
+
+    return file_list
+
+def copyto(f, outdir, fname):
+    '''Copy a local file (f is the full pathname) to the android device
+       at android location outdir, android new filename fname.
+    '''
+    subprocess.call(["adb", "push", f, posixpath.join(strip_schema(outdir),
+                                                      fname)])
+
+if __name__ == "__main__":
+    # copyto('/home/akkana/POD/Science/Story_Collider/249076872-the-story-collider-jonaki-bhattacharyya-losing-control.mp3', 'android:/mnt/extSdCard/Music/Podcasts', '16-05-99-so-special.mp3')
+
+    if len(sys.argv) < 2:
+        print "Usage: %s path [path ...]" % os.path.basename(sys.argv[0])
+        sys.exit(1)
+
+    sizes = True
+
+    for path in (sys.argv[1:]):
+        files = listdir(path, sizes=sizes)
+        if sizes:
+            print "\n", path, ":"
+            for f in files:
+                print "%d\t%s" % (f[1], f[0])
+        else:
+            print path, ":", ', '.join(files)
+
