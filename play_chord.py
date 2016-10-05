7e9e92c3446f31d92043140406fc65959c973026
Akkana Peck <akkana@shallowsky.com>
2016-10-04 21:01:51 -0600

    Add waveform to all the chord types. Add a lot more tests to __main__. Lots of renaming and refactoring.

--- play_chord.py
+++ play_chord.py
@@ -14,14 +14,14 @@ Check the SoX(1) man page for more fun examples.
 """
 
 import pygame, pygame.sndarray
-import numpy
 import time, random
+import numpy
 import scipy.signal
 import sys
 
 sample_rate = 44100
 
-def sine_array(hz, peak, n_samples=sample_rate):
+def sine_wave(hz, peak, n_samples=sample_rate):
     """Compute N samples of a sine wave with given frequency and peak amplitude.
        Defaults to one second.
     """
@@ -31,16 +31,17 @@ def sine_array(hz, peak, n_samples=sample_rate):
     onecycle = peak * numpy.sin(xvalues)
     return numpy.resize(onecycle, (n_samples,)).astype(numpy.int16)
 
-def square_array(hz, peak, duty_cycle=.5, n_samples=sample_rate):
+def square_wave(hz, peak, duty_cycle=.5, n_samples=sample_rate):
     """Compute N samples of a sine wave with given frequency and peak amplitude.
        Defaults to one second.
     """
     t = numpy.linspace(0, 1, 500 * 440/hz, endpoint=False)
     wave = scipy.signal.square(2 * numpy.pi * 5 * t, duty=duty_cycle)
     wave = numpy.resize(wave, (n_samples,))
+    # Square waves sound much louder than sine, so divide peak by 2.
     return (peak / 2 * wave.astype(numpy.int16))
 
-def sawtooth_array(hz, peak, rising_ramp_width=1, n_samples=sample_rate):
+def sawtooth_wave(hz, peak, rising_ramp_width=1, n_samples=sample_rate):
     """Compute N samples of a sine wave with given frequency and peak amplitude.
        Defaults to one second.
        rising_ramp_width is the percentage of the ramp spend rising:
@@ -49,56 +50,50 @@ def sawtooth_array(hz, peak, rising_ramp_width=1, n_samples=sample_rate):
     t = numpy.linspace(0, 1, 500 * 440/hz, endpoint=False)
     wave = scipy.signal.sawtooth(2 * numpy.pi * 5 * t, width=rising_ramp_width)
     wave = numpy.resize(wave, (n_samples,))
+    # Sawtooth waves sound very quiet, so multiply peak by 4.
     return (peak * 4 * wave.astype(numpy.int16))
 
 def make_chord(hz, ratios, waveform=None):
     """Make a chord based on a list of frequency ratios."""
     sampling = 4096    # or 16384
-    print "make_chord waveform", waveform
     if not waveform:
-        waveform = sine_array
+        waveform = sine_wave
     chord = waveform(hz, sampling)
     for r in ratios[1:]:
         chord = sum([chord, waveform(hz * r / ratios[0], sampling)])
     return chord
 
 # Some popular chords and their frequency ratios:
-def fundamental(hz):
+def fundamental(hz, waveform=None):
     return make_chord(hz, [1])
 
-def third(hz):
-    return make_chord(hz, [4, 5])
+def third(hz, waveform=None):
+    return make_chord(hz, [4, 5], waveform)
 
-def fifth(hz):
-    return make_chord(hz, [2, 3])
+def fifth(hz, waveform=None):
+    return make_chord(hz, [2, 3], waveform)
 
-def major(hz):
-    return make_chord(hz, [4, 5, 6])
+def major_triad(hz, waveform=None):
+    return make_chord(hz, [4, 5, 6], waveform)
 
-def minor(hz, waveform=None):
+def minor_triad(hz, waveform=None):
     return make_chord(hz, [10, 12, 15], waveform)
 
 def diminished(hz, waveform=None):
-    return make_chord(hz, [160, 192, 231])
+    return make_chord(hz, [160, 192, 231], waveform)
 
 def seventh(hz, waveform=None):
-    return make_chord(hz, [20, 25, 30, 36])
+    return make_chord(hz, [20, 25, 30, 36], waveform)
 
 def minor_seventh(hz, waveform=None):
-    return make_chord(hz, [10, 12, 15, 18])
+    return make_chord(hz, [10, 12, 15, 18], waveform)
 
 def major_seventh(hz, waveform=None):
-    return make_chord(hz, [8, 10, 12, 15])
+    return make_chord(hz, [8, 10, 12, 15], waveform)
 
-def brass(hz):
-    "Compute a sound with some odd harmonics. Doesn't really sound brassy."
-    return sum([sine_array(hz, 4096),
-                sine_array(hz * 3, 4096),
-                sine_array(hz * 5, 4096)])
-
-def play_for(sample_array, ms):
-    "Play given samples, as a sound, for N ms."
-    sound = pygame.sndarray.make_sound(sample_array)
+def play_for(sample_wave, ms):
+    """Play given samples, as a sound, for ms milliseconds."""
+    sound = pygame.sndarray.make_sound(sample_wave)
     sound.play(-1)
     pygame.time.delay(ms)
     sound.stop()
@@ -110,51 +105,85 @@ def main():
     pygame.init()
 
     print "Sine"
-    play_for(sine_array(440, 4096), length)
+    play_for(sine_wave(440, 4096), length)
     pygame.time.delay(wait)
+
     print "Square"
-    play_for(square_array(440, 4096), length)
+    play_for(square_wave(440, 4096), length)
     pygame.time.delay(wait)
     print "Higher Square"
-    play_for(square_array(880, 4096), length)
+    play_for(square_wave(880, 4096), length)
 
     pygame.time.delay(wait)
     print "Triangle"
-    play_for(sawtooth_array(440, 4096, .5), length)  # triangle wave
+    play_for(sawtooth_wave(440, 4096, .5), length)  # triangle wave
     pygame.time.delay(wait)
     print "Sawtooth"
-    play_for(sawtooth_array(440, 4096, 1.), length)   # sawtooth wave
+    play_for(sawtooth_wave(440, 4096, 1.), length)   # sawtooth wave
 
     pygame.time.delay(wait)
-    print "Brass"
-    play_for(brass(440), length)
+    print "Fundamental (sine)"
+    play_for(fundamental(440), length)
+    pygame.time.delay(wait)
+    print "Third"
+    play_for(third(440), length)
+    pygame.time.delay(wait)
+    print "Third, square"
+    play_for(third(440, square_wave), length)
+    pygame.time.delay(wait)
+    print "Third, sawtooth"
+    play_for(third(440, sawtooth_wave), length)
+    pygame.time.delay(wait)
+    print "Fifth"
+    play_for(fifth(440), length)
 
-    # play_for(fundamental(440), length)
-    # play_for(third(440), length)
-    # play_for(fifth(440), length)
+    # Triads mostly don't work with sawtooth waves.
+    # Maybe it's something to do with the number of samples
+    # making some of the overtones cancel out.
 
     pygame.time.delay(wait)
-    print "Major"
-    play_for(major(440), length)
+    print "Major triad"
+    play_for(major_triad(440), length)
     pygame.time.delay(wait)
-    print "Minor"
-    play_for(minor(440), length)
+    print "Major triad, square"
+    play_for(major_triad(440, square_wave), length)
+    pygame.time.delay(wait)
+    print "Major triad, sawtooth"
+    play_for(major_triad(440, sawtooth_wave), length)
+    pygame.time.delay(wait)
+    print "Minor triad"
+    play_for(minor_triad(440), length)
     pygame.time.delay(wait)
     print "Minor with square wave"
-    play_for(minor(440, square_array), length)
+    play_for(minor_triad(440, square_wave), length)
 
     pygame.time.delay(wait)
     print "Diminished"
     play_for(diminished(440), length)
+
     pygame.time.delay(wait)
     print "Seventh"
     play_for(seventh(440), length)
     pygame.time.delay(wait)
+    print "Minor seventh"
+    play_for(minor_seventh(440), length)
+
+    pygame.time.delay(wait)
+    print "Minor seventh, square"
+    play_for(minor_seventh(440, square_wave), length)
+    pygame.time.delay(wait)
     print "Minor seventh, sawtooth"
-    play_for(minor_seventh(440, sawtooth_array), length)
+    play_for(minor_seventh(440, sawtooth_wave), length)
+
+    pygame.time.delay(wait)
+    print "Major seventh"
+    play_for(major_seventh(440), length)
     pygame.time.delay(wait)
     print "Major seventh, square"
-    play_for(major_seventh(440, square_array), length)
+    play_for(major_seventh(440, square_wave), length)
+    pygame.time.delay(wait)
+    print "Major seventh, sawtooth"
+    play_for(major_seventh(440, sawtooth_wave), length)
 
 if __name__ == '__main__':
     main()
