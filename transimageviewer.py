50cd996d794b6aad9601252861029fadc9271e33
Akkana Peck <akkana@shallowsky.com>
2017-04-06 11:35:52 -0600

    Remove nonworking cairo bitmap code, pare down to simple example. I'll put the bitmap code in a blog post

--- transimageviewer.py
+++ transimageviewer.py
@@ -32,61 +32,6 @@ class TransImageViewerWindow(ImageViewerWindow):
         else:
             print "Not composited"
 
-    def shapeinput_mask(self):
-        self.pixmap = gtk.gdk.Pixmap(None, self.width, self.height, 1)
-        ctx = self.pixmap.cairo_create()
-        #self.bgpb = gtk.gdk.pixbuf_new_from_file('base.png')
-        ctx.save()
-        ctx.set_source_rgba(1, 1, 1,0)
-        ctx.set_operator (cairo.OPERATOR_SOURCE)
-        ctx.paint()
-        ctx.restore()
-
-        # https://lists.cairographics.org/archives/cairo/2006-February/006362.html
-        def make_bitmap(circle=False):
-            bitmap = gtk.gdk.Pixmap(None, self.width, self.height, 1)
-            cr = bitmap.cairo_create()
-
-
-            # If requested, make a white circle in a black rect:
-            if circle:
-                # fill bitmap with black
-                cr.rectangle (0, 0, self.width, self.height)
-                cr.set_operator(cairo.OPERATOR_CLEAR)
-                cr.fill();
-
-                # draw white filled circle
-                cr.arc(self.width / 2, self.height / 2, self.width / 4,
-                       0, 2 * math.pi);
-                cr.set_operator(cairo.OPERATOR_OVER);
-                cr.fill();
-            # else make an all-white rect:
-            else:
-                # fill bitmap with white
-                cr.rectangle (0, 0, self.width, self.height)
-                cr.set_operator(cairo.OPERATOR_CLEAR)
-                cr.fill();
-
-                # Draw black up near the top so we can still have a titlebar
-                cr.rectangle (0, 0, self.width, 50)
-                cr.set_operator(cairo.OPERATOR_OVER)
-                cr.fill();
-
-            return bitmap
-
-        circle_bitmap = make_bitmap(True)
-        rect_bitmap = make_bitmap(False)
-
-        if self.is_composited():
-            print "It's composited"
-            ctx.rectangle(0, 0, 800, 600)
-            ctx.fill()
-            self.window.shape_combine_mask(circle_bitmap, 0, 0)
-            self.window.input_shape_combine_mask(rect_bitmap, 0, 0)
-        else:
-            print "Not composited"
-            self.window.shape_combine_mask(shape_bitmap, 0, 0)
-
     def key_press_event(self, widget, event):
         '''Handle a key press event anywhere in the window'''
         if event.string == " ":
