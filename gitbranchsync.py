112b6e6b093a3013e745c911cf2ee741b05507b6
Akkana Peck <akkana@shallowsky.com>
2017-07-29 17:50:47 -0600

    Exit with number of commits different in -c case. Run -cl by default. Clean up output.

--- gitbranchsync.py
+++ gitbranchsync.py
@@ -38,7 +38,7 @@ def comprefs(ref):
     upstream = ref.tracking_branch()
     if not upstream:
         # print("No upstream for " + ref.name)
-        return None, None
+        return 0, 0
 
     # The trick is to find the last SHA that's in both of ref and upstream.
     # Then figure out if either one has anything more recent.
@@ -48,38 +48,61 @@ def comprefs(ref):
                 return i, j
 
     # If we get here, there's no common element between the two.
-    return None, None
+    print("Warning: no common commit between %s and %s" % (ref.name,
+                                                           upstream.name))
+    return 0, 0
 
 def check_push_status(repo):
     '''Does this repo have changes that haven't been pushed upstream?
+       Print any info, but also return the number of changes that differ.
     '''
+    differences = 0
+
     # git status --porcelain -uno
-    print("Locally modified files:")
     porcelain = repo.git.status(porcelain=True).splitlines()
     for l in porcelain:
         if not l.startswith("?? "):
+            if not differences:
+                print("Need to commit locally modified files:")
             print l
-    print("")
+            differences += 1
+    if differences:
+        print("")
+
+    needspush = False
+    for ref in repo.heads:
+        l, r = comprefs(ref)
+        if l or r:
+            if not needspush:
+                print("Need to push:")
+                needspush = True
+            upstream = ref.tracking_branch()
+        if l > 0:
+            print("  %s is ahead of %s by %d commits" % (ref.name,
+                                                         upstream.name, l))
+        if r > 0:
+            print("  %s is ahead of %s by %d commits" % (upstream.name,
+                                                         ref.name, r))
+        differences += l + r
 
     # git for-each-ref --format="%(refname:short) %(push:track)" refs/heads | fgrep '[ahead'
     foreachref = repo.git.execute(['git', 'for-each-ref',
                                    '--format="%(refname:short) %(push:track)"',
                                    'refs/heads']).splitlines()
     # git.execute weirdly adds " at the beginning and end of each line.
-    print("for-each-ref says:")
+    foundref = False
     for l in foreachref:
-        if l.startswith('"') and l.endswith('"'):
-            l = l[1:-1]
-        print("  " + l)
-    print("")
+        if '[ahead' in l:
+            if l.startswith('"') and l.endswith('"'):
+                l = l[1:-1]
+            if not foundref:
+                print("for-each-ref says:")
+                foundref = True
+            print("  " + l)
+    if foundref:
+        print("")
 
-    print("Need to push?")
-    for ref in repo.heads:
-        l, r = comprefs(ref)
-        if l > 0:
-            print("  %s: local is ahead by %d commits" % (ref.name, l))
-        if r > 0:
-            print("  %s: remote is ahead by %d commits" % (ref.name, r))
+    return differences
 
 def list_branches(repo, add_tracking=False):
     '''List branches with their tracking info. If add_tracking is True,
@@ -87,6 +110,10 @@ def list_branches(repo, add_tracking=False):
     '''
 
     remotebranches = {}
+    if not repo.remotes:
+        print("No remotes for repo %s" % repo.working_dir)
+        return
+
     for branch in repo.remotes[0].refs:
         simplename = branch.name.split('/')[-1]
         if simplename == "HEAD":
@@ -115,17 +142,23 @@ def list_branches(repo, add_tracking=False):
             print("  %s -> %s" % (lb.name, lb.tracking_branch().name))
         else:
             print("  " + lb.name)
-    print("")
 
-    print("Branches without tracking:")
+    locals_need_tracking = False
     # Print local branches that don't track any remote branch.
     # Don't do anything about this, though.
     for name in localbranchnames - remotebranchnames:
         if not localbranches[name].tracking_branch():
+            if not locals_need_tracking:
+                print("\nLocal branches without tracking:")
+                locals_need_tracking = True
             print("  %s doesn't have a corresponding remote branch" % name)
 
     # What remote branches aren't tracked at all?
+    remotes_need_tracking = False
     for name in remotebranchnames - remotetracks:
+        if not remotes_need_tracking:
+            print("\nRemote branches that aren't tracked:")
+            remotes_need_tracking = True
         print("  %s isn't tracked by a local branch"
               % remotebranches[name].name)
         if name in localbranches:
@@ -184,21 +217,31 @@ def main():
 
     args = parser.parse_args()
 
+    # By default (no arguments specified), do -cl, i.e. check and list.
+    if not args.list and not args.check and not args.track:
+        args.list = True
+        args.check = True
+
     repo = Repo(args.repo)
 
+    retval = 0
+
+    # Fetch, if specified, always needs to happen first.
     if args.fetch:
         fetch_from_upstream(repo)
 
+    # If we're adding tracking info, do that first so check and list
+    # will reflect what we changed.
     if args.track:
         list_branches(repo, True)
-    elif args.list:
-        list_branches(repo, False)
-    elif args.check:
-        check_push_status(repo)
-    else:
-        check_push_status(repo)
-        print("")
+
+    if args.check:
+        retval = check_push_status(repo)
+
+    if args.list:
         list_branches(repo, False)
 
+    return retval
+
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
