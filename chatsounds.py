e6b48c2c9e63a836465e794cdbb9f9bd1086b04e
Akkana Peck <akkana@shallowsky.com>
2018-12-18 19:55:57 -0700

    chatsounds: add ##linux to list of high-traffic silenced channels

--- chatsounds.py
+++ chatsounds.py
@@ -20,7 +20,7 @@ NORMAL_SOUND = "pop.wav"
 SUBTLE_SOUND = "SingleClick.wav"
 SPECIAL_SOUND = "akk.wav"
 # Channels silenced by default, because they're too active:
-SILENCED_CHANNELS = [ '#twitter_akkakk', '#python' ]
+SILENCED_CHANNELS = [ '#twitter_akkakk', '#python', '##linux' ]
 # End configuration.
 
 # The debugging log file.
