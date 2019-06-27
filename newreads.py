cc1d29a7a7a20a83a78a51a37eafa22ec9fa895d
Akkana Peck <akkana@shallowsky.com>
2019-06-27 17:01:24 -0600

    newreads: print the goodreads id and allow asking for the description

--- newreads.py
+++ newreads.py
@@ -36,6 +36,55 @@ from bs4 import BeautifulSoup
 import argparse
 import sys, os
 
+class Book:
+    monthnames = [ '?', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                   'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ]
+
+
+    def __init__(self, id, title, authorlist, description,
+                 pub_year, pub_month):
+        self.id = int(id)
+        self.title = title
+        self.authors = authorlist
+        self.desc = description
+        try:
+            self.pub_year = int(pub_year)
+        except:
+            self.pub_year = 0
+        try:
+            self.pub_month = int(pub_month)
+        except:
+            self.pub_month = 0
+
+        # Things that can be filled in later, which may or may not
+        # be available:
+        self.language = None
+        self.isbn = None
+        self.isbn13 = None
+
+
+    # Books need to be sortable by publication date
+    def __lt__(self, other):
+        if self.pub_year < other.pub_year:
+            return True
+        if self.pub_year == other.pub_year and self.pub_month < other.pub_month:
+            return True
+        if self.pub_year == other.pub_year and \
+           self.pub_month == other.pub_month:
+            return self.id < other.id
+        return False
+
+
+    def __repr__(self):
+        if self.pub_month:
+            return '%s (%s %d) (Goodreads %s)'\
+                % (self.title,
+                   Book.monthnames[self.pub_month],
+                   self.pub_year, self.id)
+        return '%s (%d) (Goodreads %s)' \
+            % (self.title, self.pub_year, self.id)
+
+
 class GoodreadsAPI:
     def __init__(self):
         # Read keys
@@ -58,6 +107,33 @@ class GoodreadsAPI:
         #     print("API key is there, but won't be able to write")
 
 
+    def book_description(self, bookid):
+        # The reviews page includes book description and language
+        url = 'https://www.goodreads.com/book/show/%d.xml?key=%s' \
+            % (bookid, self.keys['key'])
+        print("url", url)
+
+        r = requests.get(url)
+        if r.status_code != 200:
+            raise RuntimeError("Bad status %d on %s" % (r.status_code, url))
+
+        # print(r.text)
+        soup = BeautifulSoup(r.text, 'lxml-xml')
+
+        authorstag = soup.find('authors')
+        authors = []
+        for authortag in authorstag.findAll('author'):
+            authors.append(authortag.find('name').text)
+
+        title = soup.find('title').text
+
+        desc = soup.find('description')
+        # print("description tag:", desc)
+        desc = desc.text
+
+        return title + ", by " + ','.join(authors) + '\n' + desc
+
+
     def books_by_author(self, authorname):
         '''Find books by all authors matching the given name.
            authorname is a string like "Connie Willis")
@@ -71,17 +147,18 @@ class GoodreadsAPI:
 
         url = "https://www.goodreads.com/api/author_url/%s?key=%s" \
             % (authorname, self.keys['key'])
-
         # print("URL", url)
+
         r = requests.get(url)
         if r.status_code != 200:
             raise RuntimeError("Bad status %d on %s" % (r.status_code, url))
 
         # This returns XML that includes <author id="NNNNN">
-        soup = BeautifulSoup(r.text, 'lxml')
+        soup = BeautifulSoup(r.text, 'lxml-xml')
         author_tags = soup.findAll('author')
         if not author_tags:
-            return None
+            print("No authors found matching '%s'" % authorname)
+            return None, None
 
         booklists = []
 
@@ -100,6 +177,7 @@ class GoodreadsAPI:
                 url = "https://www.goodreads.com/author/list/%s" \
                       "?format=xml&key=%s&page=%d" \
                       % (author_id, self.keys['key'], page)
+                print("url", url)
 
                 print('%d... ' % page, end='', file=sys.stderr)
                 sys.stderr.flush()
@@ -109,7 +187,7 @@ class GoodreadsAPI:
                     raise RuntimeError("Bad status %d on %s" % (r.status_code,
                                                                 url))
 
-                soup = BeautifulSoup(r.text, 'lxml')
+                soup = BeautifulSoup(r.text, 'lxml-xml')
                 bookstag = soup.find('books')
                 if not bookstag:
                     print("Eek, author", author_id, "doesn't have any books")
@@ -121,7 +199,13 @@ class GoodreadsAPI:
 
                 for booktag in bookstag.findAll('book'):
                     title = booktag.find('title').text
-                    book_id = booktag.get('id')
+
+                    book_id = booktag.find('id').text
+
+                    # isbn isn't reliably available from author search pages
+                    # isbn = booktag.find('isbn').text
+                    # isbn13 = booktag.find('isbn13').text
+
                     publication_year = booktag.find('publication_year').text
                     try:
                         publication_year = int(publication_year)
@@ -133,6 +217,12 @@ class GoodreadsAPI:
                     except:
                         publication_month = 0
 
+                    desc = booktag.find('description')
+                    if desc:
+                        desc = desc.text
+                    else:
+                        desc = ''
+
                     # print("%s (%s %s)" % (title,
                     #                       publication_month,
                     #                       publication_year))
@@ -143,73 +233,77 @@ class GoodreadsAPI:
                     # Unfortunately, we can't just quit at that point;
                     # valid books aren't necessarily listed before bogus books.
                     bookauthors = booktag.findAll('author')
+                    authorlist = []
                     isauthor = False
                     for auth in bookauthors:
+                        authorlist.append(auth.find('name').text)
                         thisid = auth.find('id').text
                         if thisid == author_id:
                             # It's valid, the author really wrote it.
                             # Add it to the book list.
                             isauthor = True
-                            booklist.append((publication_year,
-                                             publication_month,
-                                             title, book_id))
-                            break
-                    if not isauthor:
-                        anthologies.append((publication_year,
-                                            publication_month,
-                                            title, book_id))
+
+                    if isauthor:
+                        booklist.append(Book(book_id, title,
+                                             authorlist,
+                                             desc,
+                                             publication_year,
+                                             publication_month))
+                    else:
+                        anthologies.append(Book(book_id, title,
+                                             authorlist,
+                                             desc,
+                                             publication_year,
+                                             publication_month))
 
                 if int(end) >= int(total):
-                    booklist.sort(reverse=True)
-                    anthologies.sort(reverse=True)
-                    print()
-                    return booklist, anthologies
+                    break
 
                 page += 1
 
-            # Should never get here, but you never know.
-            print("Somehow ended loop without finishing the list")
-            print("Total was %s, this page had %d books, %s - %s"
-                  % (total, len(bookstag), start, end))
-            booklist.sort(reverse=True)
-            anthologies.sort(reverse=True)
-            print()
-            return booklist, anthologies
+        booklist.sort(reverse=True)
+        anthologies.sort(reverse=True)
+        return booklist, anthologies
 
-
-monthnames = [ '?', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-               'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ]
-
-
-def bookline(book):
-    if not book[1]:
-        return '%s (%d)' % (book[2], book[0])
-    return '%s (%s %d) (Goodreads %s)' % (book[2], monthnames[book[1]],
-                                          book[0],
-                                          book[3])
 
 if __name__ == '__main__':
+
+    # book2019 = Book(1234, 'Book 2019', ['a b'], '', 2019, 0)
+    # book2019_2 = Book(1234, 'Book 2 2019', ['a b'], '', 2019, 2)
+    # book2017 = Book(1234, 'Book 2017', ['a b'], '', 2017, 5)
+    # book2014 = Book(1234, 'Book 2014', ['a b'], '', 2014, 9)
+    # books = [ book2017, book2019, book2014, book2019_2 ]
+    # print(sorted(books))
+    # sys.exit(0)
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-a', action="store_true", dest="anthologies",
                         help='Include anthologies that include this author')
     parser.add_argument('-y', action="store", dest="year", type=int,
                         help='year: Show only books from this year or later')
-    parser.add_argument('authors', nargs='+', help="Authors")
+    parser.add_argument('-d', action="store", dest="desc", type=int,
+                        help='description: show Goodreads description for this book')
+    parser.add_argument('author', nargs='*', help="Authors")
     args = parser.parse_args(sys.argv[1:])
 
     api = GoodreadsAPI()
 
-    for author in args.authors:
+    if args.desc:
+        print(api.book_description(args.desc))
+        sys.exit(0)
+
+    for author in args.author:
+        author = author.strip()
+
         booklist, anthologies = api.books_by_author(author)
 
         print("\n====", author)
 
         if args.anthologies:
             for book in anthologies:
-                if not args.year or book[0] >= args.year:
-                    print(bookline(book))
+                if not args.year or book.pub_year >= args.year:
+                    print(book)
 
         for book in booklist:
-            if not args.year or book[0] >= args.year:
-                print(bookline(book))
+            if not args.year or book.pub_year >= args.year:
+                print(book)
