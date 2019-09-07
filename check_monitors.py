e94d85a8db98f9264cf4f5200d39ea41d8bdb1d0
Akkana Peck <akkana@shallowsky.com>
2019-09-07 11:02:56 -0600

    check_monitors: keep track of monitor sizes and positions. Add code to find windows, preparatory to finding orphans

--- check_monitors.py
+++ check_monitors.py
@@ -33,36 +33,68 @@ DEBUGFILE = '/tmp/check-monitors.out'
 class XDisp:
 
     def __init__(self):
-        self.dpy = display.Display()
-        self.root = self.dpy.screen().root
-        self.resources = self.root.xrandr_get_screen_resources()._data
-
-        self.monitors = {}
+        # Display modes
         self.allmodes = {}
+
+        # Monitor objects for all the connected monitors, indexed by name
+        self.monitors = {}
+
+        # width, height and xinerama x, y for all connected monitors,
+        # indexed by name
+        self.mon_geom = {}
+
+        # If this is a laptop, the name of its native display
+        self.laptop_screen = None
 
 
     def find_monitors(self):
+        self.dpy = display.Display()
+        self.root = self.dpy.screen().root
+        print("root size", self.root.get_geometry())
+        self.resources = self.root.xrandr_get_screen_resources()._data
+
         # Accessing modes sometimes makes outputs mysteriously disappear,
         # so save outputs first.
         outputs = self.resources['outputs']
 
-        # Build up a mode table. There's probably some clever IterTools construct
-        # that could do this in one line.
+        # Build up a mode table. There's probably some clever IterTools
+        # construct that could do this in one line.
         for m in self.resources['modes']:
             self.allmodes[m['id']] = '%dx%d' % (m['width'], m['height'])
 
         # Loop over the outputs.
         for output in outputs:
-            data = self.dpy.xrandr_get_output_info(
+            mondata = self.dpy.xrandr_get_output_info(
                 output, self.resources['config_timestamp'])._data
 
-            if data['mm_width'] <= 0 or data['mm_height'] <= 0:
+            if mondata['mm_width'] <= 0 or mondata['mm_height'] <= 0:
                 # Not an actual monitor; I'm not sure what these are for
                 # but they don't seem to have any useful info
                 continue
 
-            name = data['name']
-            self.monitors[data['name']] = data
+            name = mondata['name']
+            self.monitors[name] = mondata
+            if name.startswith('eDP') or name.startswith('LVDS'):
+                self.laptop_screen = name
+                print("laptop monitor:", name)
+
+            # Figure out if it's cloned or extended, and its xinerama position
+            # https://stackoverflow.com/questions/49136692/python-xlib-how-to-deterministically-tell-whether-display-output-is-in-extendi
+            # which references https://www.x.org/wiki/Development/Documentation/HowVideoCardsWork/#index3h3
+            # crtcInfo also includes rotation info but I'm not doing anything
+            # with that since I don't personally use it.
+            crtcInfo = self.dpy.xrandr_get_crtc_info(mondata['crtc'],
+                                            self.resources['config_timestamp'])
+            # print(crtcInfo)
+
+            self.mon_geom[name] = {
+                'x': crtcInfo.x,
+                'y': crtcInfo.y,
+                'width': crtcInfo.width,
+                'height': crtcInfo.height,
+                'mm_width': mondata['mm_width'],
+                'mm_height': mondata['mm_height']
+            }
 
 
     def print_monitor(self, mon, show_all_modes):
@@ -70,29 +102,67 @@ class XDisp:
             print("\n%s:" % mon['name'])
             print(", ".join([self.allmodes[m] for m in mon['modes']]))
 
-        # Figure out if it's cloned or extended, and its xinerama position
-        # https://stackoverflow.com/questions/49136692/python-xlib-how-to-deterministically-tell-whether-display-output-is-in-extendi
-        # which references https://www.x.org/wiki/Development/Documentation/HowVideoCardsWork/#index3h3
-        try:
-            crtcInfo = self.dpy.xrandr_get_crtc_info(mon['crtc'],
-                                            self.resources['config_timestamp'])
-            # print(crtcInfo)
-            # crtcInfo also includes rotation info but I'm not doing anything
-            # with that since I don't personally use it.
-            x = crtcInfo.x
-            y = crtcInfo.y
-            if not show_all_modes:
-                print("%s: " % mon['name'], end='')
-            print("Size: %dx%d Position: (%d, %d)" % (crtcInfo.width,
-                                                      crtcInfo.height,
-                                                      crtcInfo.x, crtcInfo.y))
-        except XError:
-            print("    Xlib error")
+        geom = self.mon_geom[mon['name']]
+        if self.laptop_screen == mon['name']:
+            islaptop = "    **laptop"
+        else:
+            islaptop = ""
+        print("Size: %4dx%4d   Position: (%4d, %4d)   mm: %d x %d%s"
+              % (geom['width'], geom['height'], geom['x'], geom['y'],
+                 geom['mm_width'], geom['mm_height'],
+                 islaptop))
 
 
     def print_monitors(self, show_all_modes):
         for mname in self.monitors:
             self.print_monitor(self.monitors[mname], show_all_modes)
+
+
+    def move_window(self, win, newx, newy):
+        '''Move a window so it's visible on the screen.
+        '''
+        geom = win.get_geometry()
+        if win.get_wm_name() == WINDOW_NAME:
+            print("Current size", geom.width, geom.height)
+
+            print('Moving Win')
+            win.configure(x=newx, y=newy,
+                          width=geom.width, height=geom.height,
+                          # border_width=0,
+                          stack_mode=Xlib.X.Above)
+            dpy.sync()
+        else:
+            print("%s: %dx%d +%d+%d" % (win.get_wm_name(),
+                                        geom.width, geom.height,
+                                        geom.x, geom.y))
+
+
+    def find_orphans(self):
+        print("Trying to find orphans")
+
+        window_ids = self.root.get_full_property(
+            self.dpy.intern_atom('_NET_CLIENT_LIST'), X.AnyPropertyType
+        ).value
+        for win_id in window_ids:
+            win = self.dpy.create_resource_object('window', win_id)
+
+            geom = win.get_geometry()
+
+            print("%4dx%4d +%d+%d   %s" % (geom.width, geom.height,
+                                           geom.x, geom.y,
+                                           win.get_wm_name()))
+
+
+
+    def move_orphan(self, win, geom, newx, newy):
+        print("Moving %s from %d, %d. Current size %dx%d"
+              % (win.name, geom.x, geom.y, geom.width, geom.height))
+
+        win.configure(x=newx, y=newy,
+                      width=geom.width, height=geom.height)
+                      # border_width=0,
+                      # stack_mode=Xlib.X.Above)
+        dpy.sync()
 
 
 if __name__ == '__main__':
@@ -108,11 +178,17 @@ if __name__ == '__main__':
     parser.add_argument('-a', "--allmodes", dest="show_all_modes",
                         default=False, action="store_true",
                         help="Show all modes allowed for each monitor")
+    parser.add_argument('-o', "--orphans", dest="orphans", default=False,
+                        action="store_true",
+                        help="Find orphaned windows that are no longer visible")
 
     args = parser.parse_args(sys.argv[1:])
 
     if args.switch:
         print("Would switch!")
+
+    elif args.orphans:
+        xdisp.find_orphans()
 
     else:
         xdisp.print_monitors(args.show_all_modes)
