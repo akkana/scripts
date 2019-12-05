5f145af03d428e715ef2139b459bcd6f29a81b43
Akkana Peck <akkana@shallowsky.com>
2019-12-05 07:26:27 -0700

    chatsounds: silence #ubuntu by default, another high-traffic channel

--- chatsounds.py
+++ chatsounds.py
@@ -20,7 +20,7 @@ NORMAL_SOUND = "pop.wav"
 SUBTLE_SOUND = "SingleClick.wav"
 SPECIAL_SOUND = "akk.wav"
 # Channels silenced by default, because they're too active:
-SILENCED_CHANNELS = [ '#twitter_akkakk', '#python', '##linux' ]
+SILENCED_CHANNELS = [ '#twitter_akkakk', '#python', '##linux', '#ubuntu' ]
 # End configuration.
 
 # The debugging log file.
