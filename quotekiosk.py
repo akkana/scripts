e9d48a3e1895e00ef2ed205f8ffb9f9e5872c418
Akkana Peck <akkana@shallowsky.com>
2020-01-17 12:08:29 -0700

    quotekiosk: Convert HTML to text, handle images, argparse, lots of other improvements

--- quotekiosk.py
+++ quotekiosk.py
@@ -15,30 +15,80 @@ from gi.repository import GLib
 import cairo
 from gi.repository import Pango
 from gi.repository import PangoCairo
+from gi.repository import GdkPixbuf
 
 import random
+import re
+from pathlib import Path
 
-class ParagraphWindow(Gtk.Window):
-    def __init__(self, width=1024, height=768, fullscreen=False,
+import html2text
+# html2text adds line breaks unless told not to:
+html_converter = html2text.HTML2Text()
+html_converter.body_width = 0
+
+IMAGE_EXTS = ( '.jpg', '.png', '.gif', '.tif' )
+
+# Fraction of screen images should take up:
+FILLFRAC = .95
+
+
+def is_html_file(filename):
+    filename = filename.lower()
+    return filename.endswith('.html') or filename.endswith('.htm')
+
+
+class AutoSizerWindow(Gtk.Window):
+    """A window that can resize its content, either text or image,
+       to fill as much space as possible.
+    """
+    def __init__(self, fullscreen=False, fontname="Serif Italic",
                  border_size=40):
-        super(ParagraphWindow, self).__init__()
+        super(AutoSizerWindow, self).__init__()
 
         self.content_area = None
 
-        self.width = width
-        self.height = height
+        self.fontname = fontname
+
         self.use_fullscreen = fullscreen
+        if not fullscreen:
+            self.width=1024
+            self.height=768
 
         self.border_size = border_size
 
         self.background_color = (0, 0, 0)
         self.text_color = (1, 1, 0)
 
-        self.quote = "test 1 2 3"
+        self.content = "*"
+        self.pixbuf = None
 
         self.content_area = Gtk.DrawingArea()
 
         self.add(self.content_area)
+
+    def set_content(self, newcontent):
+        self.pixbuf = None
+        contentpath = Path(newcontent)
+        if contentpath.exists():
+            ext = contentpath.suffix.lower()
+            if ext.startswith('.htm'):
+                try:
+                    with open(newcontent) as qf:
+                        self.content = qf.read()
+                    self.content = html_converter.handle(self.content)
+
+                except Exception as e:
+                    print("Couldn't read", newcontent, ":", e)
+                    self.content = newcontent
+
+            elif ext in IMAGE_EXTS:
+                try:
+                    self.prepare_image(newcontent)
+                except Exception as e:
+                    print("Couldn't open image", newcontent, ":", e)
+                    self.content = newcontent
+
+        self.content = self.content.strip()
 
     def show_window(self):
         if self.use_fullscreen:
@@ -53,13 +103,11 @@ class ParagraphWindow(Gtk.Window):
 
         self.show_all()
 
-    def draw_paragraph(self, paragraph, ctx):
-        """Draw the paragraph as large as can fit."""
+    def draw_text(self, paragraph, ctx):
+        """Draw the text as large as can fit."""
 
         # Clear the page
-        ctx.set_source_rgb(*self.background_color)
-        ctx.rectangle(0, 0, self.width, self.height)
-        ctx.fill()
+        self.clear(ctx)
         ctx.set_source_rgb(*self.text_color)
 
         layout = PangoCairo.create_layout(ctx)
@@ -68,15 +116,14 @@ class ParagraphWindow(Gtk.Window):
         # For some reason pango width has to be 1024 times the width.
         # Why? Where does this 1024 come from?
         # No one explains this anywhere.
-        # layout.set_width(self.width * self.get_allocation().width)
         layout.set_width(1024 * (self.width - self.border_size*2))
         layout.set_wrap(Pango.WrapMode.WORD_CHAR)
 
         fontsize = 100
 
         while fontsize:
-            fontname = "Serif Italic %d" % fontsize
-            desc = Pango.font_description_from_string(fontname)
+            font = f"{self.fontname} {fontsize}"
+            desc = Pango.font_description_from_string(font)
             layout.set_font_description(desc)
             pxsize = layout.get_pixel_size()
             if pxsize.width < self.width - self.border_size*2 and \
@@ -88,75 +135,119 @@ class ParagraphWindow(Gtk.Window):
         PangoCairo.show_layout (ctx, layout)
         # self.ctx.show_text(label)
 
-    def draw(self, widget, ctx):
-        """Draw everything: the analemma and all the labels.
-           If background isn't passed, we'll default to
-           self.background_color (opaque blue), but save_image()
-           will pass in a transparent background.
+    def prepare_image(self, filename):
+        self.pixbuf = GdkPixbuf.Pixbuf.new_from_file(filename)
+        imgW = self.pixbuf.get_width()
+        imgH = self.pixbuf.get_height()
+
+        # Check aspect ratios:
+        if imgW/imgH > self.width/self.height:
+            # image has a wider aspect ratio than window; scale by width
+            newW = self.width * FILLFRAC;
+            newH = imgH * self.width / imgW * FILLFRAC;
+        else:
+            # image has a narrower aspect ratio than window; scale by height
+            newH = self.height * FILLFRAC;
+            newW = imgW * self.height / imgH * FILLFRAC;
+
+        self.pixbuf = self.pixbuf.scale_simple(newW, newH,
+                                               GdkPixbuf.InterpType.BILINEAR)
+
+    def draw_image(self, ctx):
+        imgW = self.pixbuf.get_width()
+        imgH = self.pixbuf.get_height()
+        x = (self.width - imgW) / 2
+        y = (self.height - imgH) / 2
+
+        self.clear(ctx)
+        Gdk.cairo_set_source_pixbuf(ctx, self.pixbuf, x, y)
+
+        ctx.paint()
+
+    def clear(self, ctx):
+        """Clear the screen. XXX eventually this should fade.
         """
+        ctx.set_source_rgb(*self.background_color)
+        ctx.rectangle(0, 0, self.width, self.height)
+        ctx.fill()
+
+    def draw(self, widget, ctx):
+        """Draw everything."""
+
+        self.clear(ctx)
+
+        # Get the size every time: window may have changed.
         self.width, self.height = self.get_size()
-        self.draw_paragraph(self.quote, ctx)
+
+        if self.pixbuf:
+            self.draw_image(ctx)
+        else:
+            self.draw_text(self.content, ctx)
 
     def key_press(self, widget, event):
         """Handle a key press event anywhere in the window"""
-        # Note: to handle just printables with no modifier keys,
-        # use e.g. if event.string == "q"
 
         if event.keyval == Gdk.KEY_q:
             Gtk.main_quit()
             return
-        if event.keyval == Gdk.KEY_s and \
-           event.state & Gdk.ModifierType.CONTROL_MASK:
-            obsname = self.observer.name.replace(' ', '-')
-            self.save_image("Analemma-%s.png" % obsname, labels=False)
-            self.save_image("Analemma-%s-labels.png" % obsname, labels=True)
-            return True
+
         return False
 
-class KioskWindow(ParagraphWindow):
-    def __init__(self, width=1024, height=768, fullscreen=False,
-                 border_size=40, timeout=5):
-        super(KioskWindow, self).__init__(width, height,
-                                          fullscreen, border_size)
+
+class KioskWindow(AutoSizerWindow):
+    def __init__(self, fullscreen=False, fontname="Serif Italic",
+                 border_size=40, timeout=30):
+        super(KioskWindow, self).__init__(fullscreen,
+                                          fontname=fontname,
+                                          border_size=border_size)
         self.timeout = timeout
 
-        self.quote_list = None
+        # quote_list can be a mixture of quotes and filenames
+        self.quote_list = []
 
+        print("Setting a timeout of time", self.timeout * 1000)
         GLib.timeout_add(self.timeout * 1000, self.timeout_cb)
 
-    def set_content(self, newcontent):
+    def new_quote(self):
+        choice = random.choice(self.quote_list)
+        print("choice", choice)
+
+        self.set_content(choice)
+
+        self.content_area.queue_draw()
+
+    def add_content(self, newcontent):
         """Set the quotes that will be displayed.
-           newcontent is a list of strings.
+           newcontent is a list of strings, quotes or filenames or both.
         """
-        self.quote_list = newcontent
-        self.quote = random.choice(self.quote_list)
+        self.quote_list += newcontent
+
+        self.new_quote()
 
     def timeout_cb(self):
-        if self.quote_list:
-            self.quote = random.choice(self.quote_list)
-            self.content_area.queue_draw()
+        self.new_quote()
 
-        GLib.timeout_add(self.timeout * 1000, self.timeout_cb)
+        # Returning True keeps the timeout in effect; no need to set a new one.
+        return True
 
 
 if __name__ == "__main__":
-    def Usage():
-        progname = os.path.basename(sys.argv[0])
-        print("""Usage: %s [cityname [sun|moon]]
-       %s lat lon [sun|moon]""" % (progname, progname))
-        sys.exit(0)
-
+    import argparse
     import sys
 
-    if len(sys.argv) > 1 and sys.argv[1] == '-f':
-        win = KioskWindow(fullscreen=True)
-    else:
-        win = KioskWindow(width=1024, height=768)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', "--fullscreen", dest="fullscreen",
+                        action="store_true", default=False,
+                        help="Run fullscreen regardless of screen size")
+    parser.add_argument('-t', '--time', action="store",
+                        dest="time", type=int, default=30,
+                        help='Time in seconds to pause between quotes')
+    parser.add_argument('quotes', nargs='+', help="Quotes, or files of quotes")
+    args = parser.parse_args(sys.argv[1:])
 
-    win.set_content(["To be, or not to be--that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune Or to take arms against a sea of troubles And by opposing end them. To die, to sleep-- No more--and by a sleep to say we end The heartache, and the thousand natural shocks That flesh is heir to. 'Tis a consummation Devoutly to be wished. To die, to sleep-- To sleep--perchance to dream: ay, there's the rub, For in that sleep of death what dreams may come When we have shuffled off this mortal coil, Must give us pause. There's the respect That makes calamity of so long life.",
-                     "The quick red fox jumped over the lazy dog",
-                     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                 ])
+    win = KioskWindow(fullscreen=args.fullscreen, timeout=args.time)
+
+    win.add_content(args.quotes)
 
     win.show_window()
     Gtk.main()
