8a2675b310441f9f7ad9e077cda5f217b8292295
Akkana Peck <akkana@shallowsky.com>
2020-03-01 09:15:09 -0700

    epicycles: make AU scale and timeout configurable

--- epicycles.py
+++ epicycles.py
@@ -15,10 +15,10 @@ from gi.repository import Pango
 from gi.repository import PangoCairo
 
 import math
+import argparse
+import sys
 from datetime import datetime
 from pprint import pprint
-
-AU_SCALE = 10
 
 planets = [
     ephem.Mercury(),
@@ -39,11 +39,14 @@ def color_to_triplet(c):
     return c.red / 65535, c.green / 65535, c.blue / 65535
 
 class EclipticPoleWindow(Gtk.Window):
-    def __init__(self, time_increment=5, start_time=None):
+    def __init__(self, auscale, timestep, time_increment=5, start_time=None):
         """time_increment is in days.
            start_time is a an ephem.Date object.
         """
         super(EclipticPoleWindow, self).__init__()
+
+        self.auscale = auscale
+        self.timestep = timestep
 
         if start_time:
             self.time = start_time
@@ -54,7 +57,7 @@ class EclipticPoleWindow(Gtk.Window):
 
         # Paths for each planet. Must save the full path
         # since we might need to redraw if the window gets covered.
-        self.planet_paths = [[] for i in range(len(planets))]
+        self.planet_paths = [ [] for i in range(len(planets)) ]
 
         # Set up colors
         self.bg_color = Gdk.color_parse('black')
@@ -68,7 +71,7 @@ class EclipticPoleWindow(Gtk.Window):
         self.set_default_size(self.width, self.height)
         self.add(self.drawing_area)
 
-        GLib.timeout_add(30, self.idle_cb)
+        GLib.timeout_add(self.timestep, self.idle_cb)
 
         self.drawing_area.connect('draw', self.draw)
         # self.drawing_area.connect('configure-event', self.on_configure)
@@ -120,7 +123,7 @@ class EclipticPoleWindow(Gtk.Window):
             ra = p.ra
             dist = p.earth_distance
 
-            dist_scale = halfheight * dist / AU_SCALE
+            dist_scale = halfheight * dist / self.auscale
             x = dist_scale * math.cos(ra) + halfwidth
             y = dist_scale * math.sin(ra) + halfheight
             if self.planet_paths[i]:
@@ -147,7 +150,19 @@ class EclipticPoleWindow(Gtk.Window):
 
 
 if __name__ == '__main__':
-    win = EclipticPoleWindow()
+    parser = argparse.ArgumentParser(description="Draw planet orbits from the north ecliptic pole")
+    parser.add_argument('-a', "--au", dest="auscale", type=float, default=10,
+                        action="store",
+                        help="Scale of the window in astronomical units."
+                              " Default is 11, which shows Saturn."
+                              " 2.6 shows Mars, 30 shows Pluto.")
+    parser.add_argument('-t', "--timestep", dest="timestep",
+                        type=float, default=30,
+                        help="Time step in milliseconds (default 30)."
+                             "Controls how fast the orbits are drawn.")
+    args = parser.parse_args(sys.argv[1:])
+
+    win = EclipticPoleWindow(auscale=args.auscale, timestep=args.timestep)
 
     Gtk.main()
 
