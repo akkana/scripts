ad0144298686b44a718edf5a3c9b97f478ee5a18
Akkana Peck <akkana@shallowsky.com>
2020-05-31 19:44:01 -0600

    lowalamosmtgs: add pdfminer to read the PDF agenda. No actual parsing of the agenda yet

--- losalamosmtgs.py
+++ losalamosmtgs.py
@@ -6,12 +6,20 @@
 import requests
 from bs4 import BeautifulSoup
 import datetime
+from urllib.parse import urljoin
 
 
 def parse_meeting_list():
+    """Parse the HTML page listing meetings,
+       returning a list of dictionaries for each upcoming meeting
+       (but not past ones).
+    """
     now = datetime.datetime.now()
+    now = datetime.datetime(2020, 5, 26, 18, 53, 14, 116219)
 
-    # meeting_list_url = 'http://losalamos.legistar.com/Calendar.aspx'
+    meeting_list_url = "http://losalamos.legistar.com/Calendar.aspx"
+    # meeting_list_url_parsed = urlparse(meeting_list_url)
+
     # r = requests.get(meeting_list_url, timeout=30)
     # soup = BeautifulSoup(r.text, 'lxml')
     soup = BeautifulSoup(open("Calendar.aspx").read(), "lxml")
@@ -29,12 +37,19 @@ def parse_meeting_list():
 
     # print("Field names:", fieldnames)
 
+    upcoming = []
+
     for row in caltbl.tbody.findAll("tr"):
         dic = {}
         for i, field in enumerate(row.findAll("td")):
             if fieldnames[i].startswith("Agenda"):
+                # If there's an Agenda URL, make it absolute.
                 a = field.find("a")
-                dic[fieldnames[i]] = a.get("href")
+                href = a.get("href")
+                if href:
+                    dic[fieldnames[i]] = urljoin(meeting_list_url, href)
+                else:
+                    dic[fieldnames[i]] = None
             else:
                 dic[fieldnames[i]] = field.text.strip()
 
@@ -48,12 +63,66 @@ def parse_meeting_list():
                       "already happened")
                 continue
 
-        print(dic["Name"], "---", dic["Meeting Date"])
-        print("  Agenda:", dic["Agenda"])
-        print("  Agenda Packets:", dic["Agenda Packets"])
+        upcoming.append(dic)
+
+    return upcoming
+
+
+from pdfminer.layout import LAParams, LTTextBox
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.converter import TextConverter
+import io
+
+
+def parse_agenda(agendaloc):
+    """Parse a PDF agenda using pdfminer.
+       https://stackoverflow.com/a/56530666
+    """
+    try:
+        fh = open(agendaloc, 'rb')
+        print("Opened file", agendaloc)
+    except FileNotFoundError:
+        response = requests.get(agendaloc, stream=True)
+        # response.raw supposedly gives a file handle,
+        # but it's not seekable and pdfminer needs to seek.
+        # fh = response.raw
+        fh = io.BytesIO(response.content)
+        print("Using requests for", agendaloc)
+
+    resource_manager = PDFResourceManager()
+    fake_file_handle = io.StringIO()
+    converter = TextConverter(resource_manager, fake_file_handle,
+                              laparams=LAParams())
+    page_interpreter = PDFPageInterpreter(resource_manager, converter)
+
+    for page in PDFPage.get_pages(fh, caching=True, check_extractable=True):
+        page_interpreter.process_page(page)
+
+    text = fake_file_handle.getvalue()
+
+    # close open handles
+    fh.close()
+    converter.close()
+    fake_file_handle.close()
+
+    print(text)
 
 
 if __name__ == '__main__':
-            parse_meeting_list()
+    upcoming = parse_meeting_list()
+
+    print("\nUpcoming meetings:")
+    for mtg in upcoming:
+        print(mtg["Name"], "---", mtg["Meeting Date"])
+        print("    Agenda:", mtg["Agenda"])
+        print("    Agenda Packets:", mtg["Agenda Packets"])
+        if mtg["Agenda"]:
+            # mtg["Agenda"] = "Agenda.pdf"
+            print("\n\n***********")
+            parse_agenda(mtg["Agenda"])
+    else:
+        print("No upcoming meetings")
 
 
