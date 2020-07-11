92c7359d8caecfd391a22541b5ca78430b8820b2
Akkana Peck <akkana@shallowsky.com>
2020-07-11 14:10:22 -0600

    comet.py: calculate rises and sets over multiple days

--- comet.py
+++ comet.py
@@ -7,8 +7,10 @@
 from skyfield.api import load, Topos
 from skyfield.data import mpc
 from skyfield.constants import GM_SUN_Pitjeva_2005_km3_s2 as GM_SUN
+from skyfield import almanac
 
 import dateutil.parser
+from datetime import timedelta
 
 import argparse
 
@@ -17,8 +19,6 @@ import sys, os
 
 with load.open(mpc.COMET_URL) as f:
     comets = mpc.load_comets_dataframe(f)
-
-print(len(comets), 'comets loaded')
 
 # Index by designation for fast lookup.
 comets = comets.set_index('designation', drop=False)
@@ -43,15 +43,17 @@ def comet_by_name(namepat):
     if len(rows) == 0:
         return None
 
-    # Lots of possible matches, so print them but return nothing.
+    # Multiple matches, so print them but return nothing.
     # pandas iterrows() returns two things but they're both the same object,
     # not an index and an object like you might expect. So ignore r.
+    print("Matches from", len(comets), 'comets loaded:')
+
     for r, row in rows.iterrows():
         print(row['designation'])
     return None
 
 
-def calc_comet(comet_df, obstime, earthcoords):
+def calc_comet(comet_df, obstime, earthcoords, numdays):
     # Generating a position.
     cometvec = sun + mpc.comet_orbit(comet_df, ts, GM_SUN)
 
@@ -64,16 +66,34 @@ def calc_comet(comet_df, obstime, earthcoords):
             elev = earthcoords[2]
         else:
             elev = 0
-        topos = Topos(latitude_degrees=earthcoords[0],
-                      longitude_degrees=earthcoords[1],
-                      elevation_m=elev)
-        observer = earth + topos
-        print("Observer at", topos.latitude, "N  ", topos.longitude, "E  ",
-              "Elevation", topos.elevation.m, "m")
+        obstopos = Topos(latitude_degrees=earthcoords[0],
+                         longitude_degrees=earthcoords[1],
+                         elevation_m=elev)
+        print("\nObserver at",
+               obstopos.latitude, "N  ", obstopos.longitude, "E  ",
+              "Elevation", obstopos.elevation.m, "m")
+        observer = earth + obstopos
 
         alt, az, distance = \
             observer.at(obstime).observe(cometvec).apparent().altaz()
-        print("alt-az:", alt, az, distance)
+        print("Altitude", alt, "     Azumuth", az, distance)
+
+        if numdays:
+            print("\nRises and sets over", numdays, "days:")
+            datetime1 = obstime.utc_datetime() - timedelta(hours=2)
+            t0 = ts.utc(datetime1)
+            t1 = ts.utc(datetime1 + timedelta(days=numdays))
+
+            alm = almanac.risings_and_settings(eph, cometvec, obstopos)
+            t, y = almanac.find_discrete(t0, t1, alm)
+
+            for ti, yi in zip(t, y):
+                alt, az, distance = \
+                    observer.at(ti).observe(cometvec).apparent().altaz()
+                t = ti.utc_datetime().astimezone()
+                print("%22s %-4s   Aziumuth %-16s  Dist %s" % (
+                    t.strftime("%Y-%m-%d %H:%M %Z"),
+                    'Rise' if yi else 'Set', str(az), str(distance)))
 
 
 def Usage():
@@ -93,6 +113,9 @@ if __name__ == '__main__':
                         help="Observer's Latitude and longitude (degrees)")
     parser.add_argument('-e', action="store", dest="elev", type=float,
                         help="Observer's elevation (meters)")
+    parser.add_argument('-d', action="store", dest="numdays",
+                        type=int, default=0,
+                        help="Number of days to show risings/settings")
     parser.add_argument("cometname",
                         help="Name (full or partial) of a comet")
     args = parser.parse_args(sys.argv[1:])
@@ -115,7 +138,8 @@ if __name__ == '__main__':
     comet_df = comet_by_name(args.cometname)
 
     if comet_df is not None:
-        print(comet_df['designation'], "    ", t.utc_iso())
-        calc_comet(comet_df, t, args.coords)
+        print(comet_df['designation'], "    ",
+              t.utc_datetime().astimezone().strftime("%Y-%m-%d %H:%M %Z"))
+        calc_comet(comet_df, t, args.coords, args.numdays)
 
 
