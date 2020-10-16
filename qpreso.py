b392864bbd1e61ef99aed4d168ec4f832dfff49f
Akkana Peck <akkana@shallowsky.com>
2020-10-15 20:34:08 -0600

    A bunch of changes to geometry on dual monitors: qt isn't good about figuring out which monitor the window is on, and will give the wrong resolution leading to the wrong window size.

--- qpreso.py
+++ qpreso.py
@@ -14,7 +14,6 @@ from PyQt5.QtCore import QUrl
 from PyQt5.QtWidgets import QApplication, QShortcut, QDesktopWidget, QMainWindow
 from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage
 
-
 class PresoView(QWebEngineView):
 
     def __init__(self, url=None, curviews=None, fullscreen=False, monitor=-1,
@@ -40,9 +39,37 @@ class PresoView(QWebEngineView):
         self.imgnum = 0
 
         # Size the audience will see (used for converting to images):
-        displaysize = QDesktopWidget().screenGeometry(-1)
-        self.displaywidth = displaysize.width()
-        self.displayheight = displaysize.height()
+        # Doesn't work, returns the geometry of monitor 0 no matter what.
+        # displaysize = QDesktopWidget().screenGeometry(-1)
+
+        # Get the screen geometry and set size and position appropriately.
+        screens = app.screens()
+
+        # monitor is the number of the monitor where the window should show up.
+        if monitor >= len(screens):
+            print("There is no monitor", monitor)
+            sys.exit(1)
+        if monitor < 0:
+            # If no monitor is specified, force 0. Otherwise qt will
+            # get it wrong, and will use the geometry of screen 0 even
+            # if the window manager puts the app on screen 1.
+            monitor = 0
+
+        geom = screens[monitor].geometry()
+
+        self.displaywidth = geom.width()
+        self.displayheight = geom.height()
+
+        # Fullscreen only in the main window, not popups which will have
+        # an about:blank URL.
+        if url and url != 'about:blank':
+            self.move(geom.left(), geom.top())
+            if (fullscreen or self.displayheight <= 768):
+                # Run fullscreen if the display is XGA or smaller
+                # and it isn't a dialog window,
+                # or if fullscreen was explicitly set.
+                # displaysize = QApplication.desktop.screenGeometry()
+                self.showFullScreen()
 
         # Size of the window we'll actually display.
         # XXX Currently assumes a projector at 1024x768
@@ -58,33 +85,14 @@ class PresoView(QWebEngineView):
             self.displayheight = int(self.displayheight * zoom)
             self.fullwidth = int(self.fullwidth * zoom)
             self.fullheight = int(self.fullheight * zoom)
-            print("Display size: %d x %d" % (self.displaywidth,
-                                             self.displayheight))
-            print("Full size: %d x %d" % (self.fullwidth,
-                                          self.fullheight))
-
-        if monitor >= 0:
-            # The number of the monitor where the window should show up
-            screens = app.screens()
-            print("screens:", screens)
-            if monitor > len(screens):
-                print("There is no monitor", monitor)
-                sys.exit(1)
-
-            geom = screens[monitor].geometry()
-            self.move(geom.left(), geom.top())
-            self.showFullScreen()
-
-        else:
-            # Run fullscreen if the display is XGA or smaller,
-            # or if fullscreen was explicitly set.
-            # displaysize = QApplication.desktop.screenGeometry()
-            if fullscreen or self.displayheight <= 768:
-                self.showFullScreen()
+            # print("Display size: %d x %d" % (self.displaywidth,
+            #                                  self.displayheight))
+            # print("Full size: %d x %d" % (self.fullwidth,
+            #                               self.fullheight))
 
         # Key bindings
         # For keys like function keys, use QtGui.QKeySequence("F12")
-        QShortcut("Ctrl+Q", self, activated=self.close)
+        QShortcut("Ctrl+Q", self, activated=self.exit)
         QShortcut("Ctrl+R", self, activated=self.reload)
 
         QShortcut("Alt+Left", self, activated=self.back)
@@ -93,6 +101,13 @@ class PresoView(QWebEngineView):
         self.resize(self.fullwidth, self.fullheight)
         if url:
             self.load(QUrl.fromUserInput(url))
+
+    def exit(self):
+        app.closeAllWindows()
+
+    # Attempt 1 at getting JS console.log messages (doesn't work):
+    def javaScriptConsoleMessage(self, level, msg, line, sourceID):
+        print(level, msg, line, sourceID)
 
 
     def createWindow(self, wintype):
@@ -116,36 +131,22 @@ class PresoView(QWebEngineView):
             v.show()
             return v
 
-    # def resizeEvent(self, e):
-    #     """This is called on window create, but not from javascript resizeTo.
-
-    #     Args:
-    #         e: The QResizeEvent
-    #     """
-    #     print("resizeEvent!", e)
-    #     print("Current geometry", self.geometry())
-    #     print("new size", e.size())
-    #     print("old size", e.oldSize())
-    #     super().resizeEvent(e)
-    #     # self._update_overlay_geometries()
-    #     # self._downloadview.updateGeometry()
-    #     # self.tabbed_browser.widget.tabBar().refresh()
-
-
-    # def event(self, ev):
-    #     print("event!", ev)
-    #     return super().event(ev)
-
 
 def parse_args():
     """Parse commandline arguments."""
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+description="""Lightweight tool for displaying HTML presentations.
+
+Tip: For debugging, run with --remote-debugging-port=[portnum]
+     and then you can get debug tools with chromium to localhost:[portnum]
+     (doesn't seem to work with Firefox's inspector)""",
+        formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('-f', "--fullscreen", dest="fullscreen", default=False,
                         action="store_true",
                         help="Run fullscreen regardless of screen size")
-    parser.add_argument('-m', '--monitor', action="store", default=-1,
+    parser.add_argument('-m', '--monitor', action="store", default=0,
                         dest="monitor", type=int,
-                        help='Run fullscreen on this monitor')
+                        help='Run fullscreen on this monitor number')
     parser.add_argument('url', help='The URL to open')
 
     args = parser.parse_known_args()[0]
@@ -167,7 +168,7 @@ if __name__ == '__main__':
     import traceback
     def excepthook(excType=None, excValue=None, tracebackobj=None, *,
                    message=None, version_tag=None, parent=None):
-        # print("exception! excValue='%s'" % excValue)
+        print("exception! excValue='%s'" % excValue)
         # logging.critical(''.join(traceback.format_tb(tracebackobj)))
         # logging.critical('{0}: {1}'.format(excType, excValue))
         traceback.print_exception(excType, excValue, tracebackobj)
