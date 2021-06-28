08ed5cf61b4724a3e22f6680cec2006d6fa27774
Akkana Peck <akkana@shallowsky.com>
2021-06-27 19:15:35 -0600

    losalamosmeetings: add a link to about.html explaining RSS

--- losalamosmtgs.py
+++ losalamosmtgs.py
@@ -337,7 +337,8 @@ def write_rss20_file(mtglist):
 <body>
 <h1>Los Alamos County Government Meetings</h1>
 As of: {gendate}
- .......... <a href="{RSS_URL}index.rss">Los Alamos Meetings RSS2.0 Feed</a>.
+ ... <a href="about.html">About Los Alamos Meetings (How to Use This Page)
+ ... <a href="{RSS_URL}index.rss">Los Alamos Meetings RSS2.0 Feed</a>.
 
 """, file=htmlfp)
 
