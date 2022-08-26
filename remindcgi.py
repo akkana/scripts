da1150df54bfc584fe15d5bc7581f79e3a6f3087
Akkana Peck <akkana@shallowsky.com>
2022-08-26 13:17:44 -0600

    remindcgi: add code to make it work from the cmdline.
    Use terminal colors and styles when in commandline mode.
    Refactor everything to allow for text and html formatter classes.

--- remindcgi.py
+++ remindcgi.py
@@ -8,33 +8,140 @@ import os, sys
 from datetime import date, datetime, timedelta, MAXYEAR
 
 
-# There doesn't seem to be any way to get the current URL in a
-# Python CGI. os.environ["REQUEST_URI"] is the closest, but it doesn't
-# have the schema or server name.
-# So build it up from the components in the environment:
-# print("SERVER_NAME:", os.environ["SERVER_NAME"], file=sys.stderr)
-# print("REQUEST_URI:", os.environ["REQUEST_URI"], file=sys.stderr)
+# termcolor.colored wasn't working, and termcolor.cprint doesn't help
+# when trying to combine several formatting items.
+# So instead, see https://stackoverflow.com/a/51708889
+class ColorFormat:
+    # Foreground
+    F_Default = "\x1b[39m"
+    F_Black = "\x1b[30m"
+    F_Red = "\x1b[31m"
+    F_Green = "\x1b[32m"
+    F_Yellow = "\x1b[33m"
+    F_Blue = "\x1b[34m"
+    F_Magenta = "\x1b[35m"
+    F_Cyan = "\x1b[36m"
+    F_LightGray = "\x1b[37m"
+    F_DarkGray = "\x1b[90m"
+    F_LightRed = "\x1b[91m"
+    F_LightGreen = "\x1b[92m"
+    F_LightYellow = "\x1b[93m"
+    F_LightBlue = "\x1b[94m"
+    F_LightMagenta = "\x1b[95m"
+    F_LightCyan = "\x1b[96m"
+    F_White = "\x1b[97m"
 
-fullurl = f'{os.environ["REQUEST_SCHEME"]}://{os.environ["SERVER_NAME"]}' \
-    f'{os.environ["REQUEST_URI"]}'
+    # Formatting
+    Bold = "\x1b[1m"
+    Dim = "\x1b[2m"
+    Italic = "\x1b[3m"
+    Underlined = "\x1b[4m"
+    Blink = "\x1b[5m"
+    Reverse = "\x1b[7m"
+    Hidden = "\x1b[8m"
 
-# Strip off the query string. urlparse or urlsplit from urllib.parse
-# seem like they ought to be the way to do that, except that they
-# give path='//cal/' so you have to do string manipulation anyway
-# to fix that. Seems like urllib.parse is more trouble than it's worth.
-baseurl = fullurl.split('?')[0]
-# print("baseurl:", baseurl, file=sys.stderr)
+    # Reset part
+    Reset_Bold = "\x1b[21m"
+    Reset_Dim = "\x1b[22m"
+    Reset_Italic = "\x1b[23m"
+    Reset_Underlined = "\x1b[24"
+    Reset_Blink = "\x1b[25m"
+    Reset_Reverse = "\x1b[27m"
+    Reset_Hidden = "\x1b[28m"
 
 
-def linkify(line):
-    line = line.strip()
-    if line.startswith('http'):
-        return '<a href="%s">%s</a>' % (line, line)
-    return line
+class TextFormatter:
+    REMINDDIR = os.path.expanduser("~/web/cal/")
+
+    @staticmethod
+    def linkify(line):
+        if is_link(line):
+            return "%s%s%s%s%s" % ( ColorFormat.F_Blue, ColorFormat.Italic,
+                                    line,
+                                    ColorFormat.Reset_Italic,
+                                    ColorFormat.F_Default )
+        return line
+
+    @staticmethod
+    def highlight(line):
+        return "%s%s%s" % ( ColorFormat.Bold,
+                            line,
+                            ColorFormat.Reset_Bold )
+        return line
+
+    @staticmethod
+    def header(line):
+        return "%s%s%s%s%s%s\n" % ( ColorFormat.F_Magenta, ColorFormat.Bold,
+                                  "**** ", line,
+                                  ColorFormat.Reset_Bold,
+                                  ColorFormat.F_Default )
+        return line
+
+    @staticmethod
+    def linebreak():
+        return ""
+
+    @staticmethod
+    def eventbreak():
+        return ""
+
+    @staticmethod
+    def print_head(title):
+        print(TextFormatter.header(title))
+
+    @staticmethod
+    def print_foot():
+        pass
 
 
-def print_head(title):
-    print(f'''Content-type: text/html
+class HTMLFormatter:
+    REMINDDIR = os.getcwd()
+
+    @staticmethod
+    def linkify(line):
+        line = line.strip()
+        if is_link(line):
+            return '<a href="%s">%s</a>' % (line, line)
+        return line
+
+    @staticmethod
+    def header(line):
+        return f"<h2>{line}</h2>"
+
+    @staticmethod
+    def highlight(line):
+        return f"<strong>{line}</strong>"
+
+    @staticmethod
+    def linebreak():
+        return "<br />"
+
+    @staticmethod
+    def eventbreak():
+        return "<p>"
+
+    @staticmethod
+    def print_head(title):
+
+        # There doesn't seem to be any way to get the current URL in a
+        # Python CGI. os.environ["REQUEST_URI"] is the closest, but it doesn't
+        # have the schema or server name.
+        # So build it up from the components in the environment:
+        # print("SERVER_NAME:", os.environ["SERVER_NAME"], file=sys.stderr)
+        # print("REQUEST_URI:", os.environ["REQUEST_URI"], file=sys.stderr)
+
+        fullurl = f'{os.environ["REQUEST_SCHEME"]}://' \
+            f'{os.environ["SERVER_NAME"]}' \
+            f'{os.environ["REQUEST_URI"]}'
+
+        # Strip off the query string. urlparse or urlsplit from urllib.parse
+        # seem like they ought to be the way to do that, except that they
+        # give path='//cal/' so you have to do string manipulation anyway
+        # to fix that. Seems like urllib.parse is more trouble than it's worth.
+        baseurl = fullurl.split('?')[0]
+        # print("baseurl:", baseurl, file=sys.stderr)
+
+        print(f'''Content-type: text/html
 
 <html>
 <head>
@@ -57,28 +164,24 @@ def print_head(title):
 <p>
 ''')
 
+    @staticmethod
+    def print_foot():
+        print(f'''
+</body>
+</html>
+''')
 
-form = cgi.FieldStorage()
-if 'when' in form:
-    when = form['when'].value
 
-    today = date.today()
-    if when.lower() == "week":
-        print_head("Upcoming Week")
-        enddate = today + timedelta(days=7)
-    elif when.lower() == "month":
-        print_head("Upcoming Month")
-        enddate = today + timedelta(days=31)
-    else:
-        # Show everything
-        if when.lower() != "all":
-            print(f"<p>\nDon't understand when '{when}';",
-                  "showing all events")
-        print_head("All Events")
-        enddate = date(MAXYEAR, 12, 31)
+def is_link(line):
+    return line.startswith('http')
 
+
+today = date.today()
+
+
+def print_calendar(enddate, formatter):
     remindout = subprocess.check_output(["/usr/bin/remind", "-n",
-                                         os.path.join(os.getcwd(),
+                                         os.path.join(formatter.REMINDDIR,
                                                       "remind.txt")])
     lines = remindout.decode().split('\n')
     # lines look like '2022/08/27 on Saturday, August 27th: dark-sky night'
@@ -93,28 +196,29 @@ if 'when' in form:
             d = datetime.strptime(line[:10], '%Y/%m/%d').date()
         except ValueError:
             print("Couldn't parse", line[:10],
-                  ": full line was '", line, "'<p>")
+                  ": full line was '", line, formatter.eventbreak())
             continue
         if d > enddate:
             break
         newmonth = d.strftime("%b")
         if newmonth != monthname:
             monthname = newmonth
-            print(f"<h2>{monthname}</h2>")
+            print(formatter.header(monthname))
         sublines = line.replace('||', '\n').split('\n')
         firstline = sublines[0][11:]
         if firstline.startswith("on "):
             firstline = firstline[3:]
-        # print(f"<b>{d.strftime('%a %b %d')} {sublines[0][11:]}</b><br>")
-        print(f"<b>{firstline}</b><br>")
+        print(formatter.highlight(firstline))
         for subline in sublines[1:]:
-            print(linkify(subline), "<br>")
-        print("<p>")
+            print(formatter.linkify(subline), formatter.linebreak())
+        print(formatter.eventbreak())
 
-else:
-    print_head("Reminders")
+    formatter.print_foot()
+
+
+def print_reminders(formatter):
     remindout = subprocess.check_output(["/usr/bin/remind", "-g",
-                                         os.path.join(os.getcwd(),
+                                         os.path.join(formatter.REMINDDIR,
                                                       "remind.txt")])
     remindout = remindout.decode().replace("||", "\n")
 
@@ -124,18 +228,59 @@ else:
         line = line.strip()
         if not line:
             new_event = True
-            print('<p>')
             continue
-        line = linkify(line)
+        line = formatter.linkify(line)
         if new_event:
-            print(f'<strong>{line}</strong>')
+            print(formatter.eventbreak(), formatter.highlight(line))
         else:
-            print(line)
-        print('<br>')
+            print(line, formatter.linebreak())
         new_event = False
 
-print(f'''
-</body>
-</html>
-''')
+    formatter.print_foot()
+
+
+if __name__ == '__main__':
+    if 'REQUEST_METHOD' in os.environ:
+        formatter = HTMLFormatter()
+
+        form = cgi.FieldStorage()
+        print("form:", form, file=sys.stderr)
+
+        if 'when' in form:
+            when = form['when'].value.lower()
+        else:
+            when = None
+    else:
+        if len(sys.argv) <= 1:
+            when = None
+        else:
+            when = sys.argv[1].lower()
+
+        formatter = TextFormatter()
+
+    if when == None:
+        # Show everything
+        formatter.print_head("Reminders")
+        enddate = date(MAXYEAR, 12, 31)
+    elif when == "week":
+        formatter.print_head("Upcoming Week")
+        enddate = today + timedelta(days=7)
+    elif when == "month":
+        formatter.print_head("Upcoming Month")
+        enddate = today + timedelta(days=31)
+    else:
+        # Show everything
+        if when != "all":
+            print(f"<p>\nDon't understand when '{when}';",
+                  "showing all events", file=sys.stderr)
+        formatter.print_head("All Events")
+        enddate = date(MAXYEAR, 12, 31)
+
+    print("Today is", today.strftime("%a, %b %d"))
+    print(formatter.linebreak())
+
+    if when:
+        print_calendar(enddate, formatter)
+    else:
+        print_reminders(formatter)
 
