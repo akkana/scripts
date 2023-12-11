d429d57be876c34df99574bcbdebe258af8e2daa
Akkana Peck <akkana@shallowsky.com>
2023-12-10 17:50:03 -0700

    waymaker: more robust parsing, and save a text file as well as GPX

--- mapping/waymaker.py
+++ mapping/waymaker.py
@@ -9,16 +9,21 @@
 """waymaker: take a file of names and descriptions
    and turn it into a collection of GPX waypoints.
 
-   file format:
+   file format: (may or may not have lat/lon already, or zip code)
 
+latitude longitude
 street address
 city, state zip
 comments comments
 more comments
 
 street address
-city, state zip
-still more comments
+city, state
+comments comments
+
+    May or may not have lat/lon already; if it does, it should be first.
+    Zip code may be missing.
+
 """
 
 import sys, os
@@ -29,9 +34,12 @@ import time
 
 import urllib.request, urllib.parse, urllib.error, json
 
+
 def write_gpx_file(entries, filename, omit_address=False, omit_time=False):
-    """Write the list of entries -- each entry is [lat, long, desc] --
+    """Write the list of entries -- each entry is a dic with keys
+       coords, addr, desc
        to a GPX file as separate waypoints.
+       entries is a list of (lat, lon, address-and-other-stuff)
     """
     fp = open(filename, 'w')
     fp.write('''<?xml version="1.0" encoding="UTF-8"?>
@@ -45,35 +53,57 @@ xsi:schemaLocation="http://www.topografix.com/GPX/1/0 http://www.topografix.com/
     if not omit_time:
         fp.write('<time>%s</time>\n' % datetime.datetime.now().isoformat())
 
-    # Calculate our bounds:
+    # Calculate bounds:
     minlat = 91
     maxlat = -90
     minlon = 1000
     maxlon = -1000
     for ent in entries:
-        if ent[0] < minlat:
-            minlat = ent[0]
-        if ent[0] > maxlat:
-            maxlat = ent[0]
-        if ent[1] < minlon:
-            minlon = ent[1]
-        if ent[1] > maxlon:
-            maxlon = ent[1]
+        if "coords" not in ent:
+            continue
+        if ent["coords"][0] < minlat:
+            minlat = ent["coords"][0]
+        if ent["coords"][0] > maxlat:
+            maxlat = ent["coords"][0]
+        if ent["coords"][1] < minlon:
+            minlon = ent["coords"][1]
+        if ent["coords"][1] > maxlon:
+            maxlon = ent["coords"][1]
 
     fp.write('<bounds minlat="%f" minlon="%f" maxlat="%f" maxlon="%f"/>\n' \
                  % (minlat, minlon, maxlat, maxlon))
 
     for ent in entries:
-        print('<wpt lat="%f" lon="%f">' % (ent[0], ent[1]), file=fp)
+        print('<wpt lat="%f" lon="%f">' % (ent["coords"][0], ent["coords"][1]),
+              file=fp)
         if omit_address:
-            addy = '\n'.join(ent[2].split('\n')[2:])
+            addy = '\n'.join(ent["addr"].split('\n')[2:])
         else:
-            addy = ent[2]
+            addy = ent["addr"]
+        if "desc" in ent:
+            addy += '\n' + ent["desc"]
+
+        # XXX Need to remove special characters XML can't handle:
+        # ? & ( ) ' "
+        addy = html.escape(addy)
+
         print('<name>%s</name>' % addy, file=fp)
         print('</wpt>', file=fp)
 
     fp.write('</gpx>\n')
     fp.close()
+
+
+def write_text_file(entries, filename):
+    with open(filename, 'w') as fp:
+        for ent in entries:
+            if "coords" in ent:
+                print(f"{ent['coords'][0]} {ent['coords'][1]}", file=fp)
+            print(ent["addr"], file=fp)
+            if "desc" in ent:
+                print(ent["desc"], file=fp)
+            print('', file=fp)
+
 
 #
 # Options for geocoding:
@@ -98,8 +128,10 @@ xsi:schemaLocation="http://www.topografix.com/GPX/1/0 http://www.topografix.com/
 
 def geocode(addr):
     """Geocode using the US Census API.
-       Returns a (lat, lon) pair, or None, None.
+       Returns (lat, lon, zip), or None, None, None.
     """
+    print("Geocoding", addr)
+
     url = 'https://geocoding.geo.census.gov/geocoder/locations/onelineaddress'
     url += '?benchmark=Public_AR_Current'
     url += '&vintage=Current'
@@ -116,10 +148,18 @@ def geocode(addr):
 
     # Weirdly, they call the coordinates x and y, not longitude/latitude
     return (float(firstmatch['coordinates']['y']),
-            float(firstmatch['coordinates']['x']))
+            float(firstmatch['coordinates']['x']),
+            firstmatch['addressComponents']['zip'])
 
 
-statezip = '.*[A-Z]{2}(\s+\d{5})*$'
+# Useful patterns
+# stateend = '.*\s+([A-Z]{2})$'
+statezip = '.*\s+([A-Z]{2})\s*(\d{5})?.*$'
+numeric = '[\+\-\d\.]'
+twonums = '^(%s+)\s+(%s+)$' % (numeric, numeric)
+
+# street directions that could be confused for a state name
+abqdirections = [ "NE", "NW", "SE", "SW" ]
 
 def read_description_file(filename):
     """Read a file filled with multi-line descriptions, blank line separated.
@@ -128,28 +168,34 @@ def read_description_file(filename):
        The next one or two lines should be a US street address,
        including zip code.
        The rest is free-form description.
-       Returns a list of entries, where each entry is a list:
-       [ latitude, longitude, text ]
+       Returns a list of entries, where each entry is a dict
+       with keys: coordinates, addr, desc
+
+       Save a modified file to filename-coords
     """
 
     entries = []
-    cur_ent = []
+    cur_ent = {}
 
     fp = open(filename)
-    for line in fp:
-        line = line.strip()
+    for origline in fp:
+        line = origline.strip()
         if not line:    # end of a record. Save the current entry and move on
             if cur_ent:
                 entries.append(cur_ent)
-                cur_ent = []
+                cur_ent = {}
             continue
 
-        if not cur_ent:
-            numeric = '[\+\-\d\.]'
+        line = re.sub('#.*$', '', line).strip()
+        if not line:
+            # Lines with only comments get skipped,
+            # but don't trigger a new entry
+            continue
+
+        if not cur_ent or "addr" not in cur_ent or not cur_ent["addr"]:
             # re.search doesn't work if you put the % expression in the
             # search call, but it does work if you store the
             # intermediate string first:
-            twonums = '^(%s+)\s+(%s+)$' % (numeric, numeric)
             match = re.search(twonums, line)
             if match:
                 # Okay, they may be numbers, but that doesn't mean
@@ -158,10 +204,8 @@ def read_description_file(filename):
                 lat = float(match.group(1))
                 lon = float(match.group(2))
                 if lat >= -90 and lat <= 90 and lon >= -180 and lon <= 360:
-                    cur_ent.append(lat)
-                    cur_ent.append(lon)
-                    # Start cur_ent[2] with a null string:
-                    cur_ent.append('')
+                    cur_ent["coords"] = lat, lon
+                    # go to the next line
                     continue
                 # If the numbers didn't pass the sanity check,
                 # fall through to the address parser.
@@ -169,11 +213,21 @@ def read_description_file(filename):
             # Now either the first line, or the first two lines,
             # are an address. But we should be able to tell them apart:
             # The last two fields of an address are a state
-            # (2 uppercase letters) followed by a 5-digit zip code.
+            # (2 uppercase letters), maybe followed by a 5-digit zip code.
             match = re.search(statezip, line)
+            # Albuquerque street addresses often end in NE, SW etc.
+            # which match the state pattern but aren't states.
+            if match and match.group(1):
+                state = match.group(1)
+                if state in abqdirections:
+                    match = None
             # If the state/zip wasn't in the first line, try the second:
             line2 = None
-            if not match:
+            if match and match.group(2):
+                # If we've seen a zip code, no need to look for further addr
+                has_zip = match.group(2)
+            else:
+                has_zip = None
                 # Try guards against StopIteration, i.e. end of file
                 try:
                     line2 = next(fp).strip()
@@ -193,6 +247,8 @@ def read_description_file(filename):
                         # Now continue the outer loop --
                         # don't try to process this blank line as an address.
                         continue
+                    else:
+                        has_zip = match.group(2)
                 except StopIteration:
                     # StopIteration means the end of the file.
                     # Since this clause is only to start a new entry,
@@ -209,40 +265,37 @@ def read_description_file(filename):
             addr = line
             if line2:
                 addr += ' ' + line2
-            print("Found an address! %s" % addr)
+            print("\nFound an address! %s" % addr)
 
-            # So instead, use a local version:
-            lat, lon = geocode(addr)
-            if not lat:
-                print("Can't geocode", addr)
-                cur_ent = []
-                continue
-
-            cur_ent.append(lat)
-            cur_ent.append(lon)
-
-            # XXX Need to remove special characters XML can't handle:
-            # ? & ( ) ' "
-
-            # and append the address as the first part of the description:
-            if line2:
-                cur_ent.append(html.escape(line) + '\n' + html.escape(line2))
+            # Were there already coordinates?
+            if "coords" not in cur_ent or not cur_ent["coords"]:
+                lat, lon, zip = geocode(addr)
+                if not lat:
+                    print("Can't geocode", addr)
+                    cur_ent = {}
+                    continue
             else:
-                cur_ent.append(html.escape(line))
+                print(addr, "Already has coordinates")
+
+            if not has_zip:
+                addr += " " + zip
+
+            cur_ent["addr"] = addr
+            cur_ent["coords"] = (lat, lon)
 
             continue
 
         # Else we have a non-null line AND we have a current entry,
-        # so we're just appending to cur_ent[2].
+        # so we're just appending to the description..
         # But skip lines that have any long words that are likely
         # too long to wrap on a phone display (they're probably URLs).
         if re.search('\S{27,}', line):
             print("Skipping long line: '%s'" % line)
             continue
-        if cur_ent[2]:
-            cur_ent[2] += '\n' + html.escape(line)
+        if "desc" in cur_ent and cur_ent["desc"]:
+            cur_ent["desc"] += '\n' + line
         else:
-            cur_ent[2] += html.escape(line)
+            cur_ent["desc"] = line
 
     if cur_ent:
         entries.append(cur_ent)
@@ -251,10 +304,12 @@ def read_description_file(filename):
 
     return entries
 
+
 def Usage():
         print("Usage: %s infile.txt outfile.gpx"
               % os.path.basename(sys.argv[0]))
         sys.exit(1)
+
 
 if __name__ == "__main__" :
     if len(sys.argv) < 3:
@@ -267,5 +322,17 @@ if __name__ == "__main__" :
         Usage()
 
     entries = read_description_file(sys.argv[1])
+    print()
+
     write_gpx_file(entries, sys.argv[2])
+    print("Wrote GPX file", sys.argv[2])
+
+    base, ext = os.path.splitext(sys.argv[2])
+    textfile = base + ".txt"
+    # Don't overwrite the input file, since write_text_file
+    # doesn't preserve comments and other formatting
+    if textfile == sys.argv[1]:
+        textfile = base + "1.txt"
+    write_text_file(entries, textfile)
+    print("Saved text file", textfile)
 
