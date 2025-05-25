60a3f4b30275f580716f0609b8dc54b935c9bdbc
Akkana Peck <akkana@shallowsky.com>
2025-05-24 19:18:47 -0600

    district_population mostly works now, but still needs some tweaking

--- mapping/district_population.py
+++ mapping/district_population.py
@@ -26,17 +26,15 @@ def population_in_districts(blockshapefile, popcsvfile, districtshapefile):
     blocks = gpd.read_file(blockshapefile)
     districts = gpd.read_file(districtshapefile)
 
+    # Every other entry in the districts file is a point, a specific school.
+    # Remove them so they don't get in the way later.
+    districts = districts[districts.geom_type != 'Point']
+
     # To calculate area or intersection, results can be wrong with EPSG:4326;
     # apparently it's better to reproject to EPSG:4326 or EPSG:6933.
     # Initially, blocks uses EPSG:4269/NAD83 and districts, EPSG:4326/WGS 84.
     blocks = blocks.to_crs(epsg=6933)
     districts = districts.to_crs(epsg=6933)
-
-    # XXX Do something here to get the total population column
-    # from popcsvfile into blocks. This doesn't work:
-    # blocks['population'] = None      # Initialize a new column in the dataframe
-    # blocks['population'] = blocks['population'].astype('int')
-    population_col = []
 
     # Fill in the population from the census file DECENNIALPL2020.P1-Data.csv.
     # DECENNIALPL2020.P1-Column-Metadata.csv says total population
@@ -47,9 +45,11 @@ def population_in_districts(blockshapefile, popcsvfile, districtshapefile):
     # Column 2, NAME, is something like
     # "Block 1000, Block Group 1, Census Tract 1, Los Alamos County, New Mexico"
 
-    # The US Census file begins with a bogus non-breaking space.
+    # The US Census file begins with a bogus non-breaking space, feff.
     # Apparently Excel on Mac sometimes adds these at the beginning
-    # of the file, for no known reason, but reading as utf-8-sig fixes it.
+    # of the file, for no known reason, but reading as utf-8-sig fixes it
+    # for the python csv module.
+    population_col = []
     with open('DECENNIALPL2020.P1-Data.csv', encoding='utf-8-sig') as fp:
         reader = csv.DictReader(fp)
         for csvline in reader:
@@ -70,28 +70,10 @@ def population_in_districts(blockshapefile, popcsvfile, districtshapefile):
 
     blocks['population'] = population_col
 
-    #
-    # print("Populations per block:")
-    # print(blocks.to_string())
-
-    # A test of one block
-    # pinonparkblock = None
-    # for b in blocks.itertuples():
-    #     if b.GEOID20 == '350280005005005':  # ID of Pinon Park block
-    #         print("Yippee!")
-    #         pinonparkblock = b
-    #         break
-
     blocks_in_districts = []
     distpops = []
     distno = 0
     for dist in districts.itertuples():
-        # Every other district in the school district file is a point
-        # indicating the location of that elementary school.
-        # We only want to work with areas.
-        if 'Polygon' not in dist.geometry.geom_type:
-            continue
-
         # Plot the intersection of the current district
         # with the collection of all the blocks:
         # intersection = dist.geometry.intersection(blocks.geometry)
@@ -102,7 +84,8 @@ def population_in_districts(blockshapefile, popcsvfile, districtshapefile):
         # else:
         #     print("District", distno, "doesn't intersect with any block")
 
-        blocklist = []
+        full_blocklist = []
+        partial_blocklist = []
         areasum = 0
         population = 0
 
@@ -112,20 +95,61 @@ def population_in_districts(blockshapefile, popcsvfile, districtshapefile):
             if not intersect:
                 continue
             print("District", distno, "contains block", b.GEOID20)
-            blocklist.append(b.BLOCKCE20)
+            # intersect is a polygon, not a dataframe, so has no .geometry
             intersecting_area = intersect.area
             areasum += intersecting_area
-            print("  b.population:", b.population)
-            print("  intersecting_area:", intersecting_area,
-                  "of", b.geometry.area)
-            population += int(b.population * intersecting_area / b.geometry.area)
+            areafrac = intersecting_area / b.geometry.area
+            print("  block population:", b.population)
+            print("  intersecting_area:", int(intersecting_area),
+                  "of", int(b.geometry.area),
+                  "=", int(areafrac * 100), "%")
+            if areafrac < .01:
+                continue
+            population_frac = int(b.population * areafrac)
+            if population_frac < .01:
+                continue
+            population += population_frac
+            if areafrac >= .99:
+                full_blocklist.append(b.BLOCKCE20)
+            else:
+                partial_blocklist.append((b.BLOCKCE20, int(areafrac * 100)))
 
-        print("district", distno, "includes blocks:", ' '.join(sorted(blocklist)))
-        print("with area", dist.geometry.area, "and population", population)
+        print("district", distno, "includes all of blocks:",
+              ' '.join(sorted(full_blocklist)))
+        if partial_blocklist:
+            partial_blocklist.sort()
+            print("plus:",
+                  ', '.join([ "%d%% of %s" % (percent, blockname)
+                              for blockname, percent in partial_blocklist ]))
+        print("  with area", dist.geometry.area, "and population", population)
         distpops.append(population)
         distno += 1
 
-    # Sanity check: does the total population work out?
+    # Sanity checks. Compare to the intersection of districts and blocks,
+    # so the first step is to calculate that intersection.
+
+    # This doesn't work: "The indices of the left and right GeoSeries' are
+    # not equal, and therefore they will be aligned"
+    # intersection = districts.geometry.intersection(blocks.geometry)
+
+    # One page said this was equivalent to intersection, but it
+    # gives an area that's 31 times too large, and when plotted,
+    # it's clear that if how="left", it's just the union of all districts,
+    #  and how="right" is the union of all blocks. Neither one
+    # takes into account both left and right.
+    # intersection = gpd.sjoin(left_df=districts, right_df=blocks,
+    #                          how="left", predicate="intersects")
+
+    # These actually seem to work:
+    intersection = districts.overlay(blocks, how='intersection')
+    # intersection = blocks.overlay(districts, how='intersection')
+
+    # intersection.plot()
+    # plt.show()
+
+    print("Sum of areas:  ", areasum)
+    print("block area:    ", sum(blocks.geometry.area))
+    print("intersect area:", sum(intersection.geometry.area))
     print("Total population:", sum(population_col))
     print("District populations", distpops, '=', sum(distpops))
 
